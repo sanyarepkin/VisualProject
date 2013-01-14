86d215e6e7cf87f5ae55383d8c94dffcb5f939af
Sanya <sanya.repkin@mail.ru>
2013-01-14 22:09:08 +0700

    Рисование линии

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,23 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rb_Cross = new System.Windows.Forms.RadioButton();
+            this.rb_line = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // rb_Cross
+            // 
+            this.rb_Cross.AutoSize = true;
+            this.rb_Cross.Location = new System.Drawing.Point(12, 295);
+            this.rb_Cross.Name = "rb_Cross";
+            this.rb_Cross.Size = new System.Drawing.Size(51, 17);
+            this.rb_Cross.TabIndex = 0;
+            this.rb_Cross.TabStop = true;
+            this.rb_Cross.Text = "Cross";
+            this.rb_Cross.UseVisualStyleBackColor = true;
+            this.rb_Cross.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
+            // 
+            // rb_line
+            // 
+            this.rb_line.AutoSize = true;
+            this.rb_line.Location = new System.Drawing.Point(12, 272);
+            this.rb_line.Name = "rb_line";
+            this.rb_line.Size = new System.Drawing.Size(45, 17);
+            this.rb_line.TabIndex = 1;
+            this.rb_line.TabStop = true;
+            this.rb_line.Text = "Line";
+            this.rb_line.UseVisualStyleBackColor = true;
+            this.rb_line.CheckedChanged += new System.EventHandler(this.rb_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(500, 324);
+            this.Controls.Add(this.rb_line);
+            this.Controls.Add(this.rb_Cross);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rb_Cross;
+        private System.Windows.Forms.RadioButton rb_line;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -11,30 +11,36 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        List<Cross> Shapes = new List<Cross>();
+        List<Shape> Shapes = new List<Shape>();
+        Point ShapeStart;
+        bool IsShapeStart = true;
         public Form1()
         {
             InitializeComponent();
         }
-
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Shape p in Shapes)
+            {
+                p.DrawWith(e.Graphics);
+            }
+        }
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             this.Text = Convert.ToString(e.Location);
         }
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            Shapes.Add(new Cross(e.X, e.Y));
-            this.Refresh();
+           if (rb_Cross.Checked) Shapes.Add(new Cross(e.X, e.Y));
+           if (rb_line.Checked)
+           {
+               if (!IsShapeStart) ShapeStart = e.Location;
+               else Shapes.Add(new Line(ShapeStart, e.Location));
+               IsShapeStart = !IsShapeStart;
+           }
         }
         
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            foreach (Cross p in Shapes)
-            {
-                p.DrawWith(e.Graphics);
-            }
-        }
-        public class Cross
+        public class Cross : Shape
         {
             int X, Y;
             Pen p = new Pen(Color.Red);
@@ -42,13 +48,33 @@ namespace WindowsFormsApplication1
             {
                 X = _X; Y = _Y;
             }
-            public  void DrawWith(Graphics g)
+            public override void DrawWith(Graphics g)
             {
                 g.DrawLine(p, X - 4, Y - 4, X + 4, Y + 4);
                 g.DrawLine(p, X + 4, Y - 4, X - 4, Y + 4);
             }
         }
+        public class Line : Shape
+        {
+            Point C, F;
+            Pen p = new Pen(Color.Blue);
+            public Line(Point _C, Point _F)
+            {
+                this.C = _C; this.F = _F;
+            }
+            public override void DrawWith(Graphics g)
+            {
+                g.DrawLine(p, C, F);
+            }
+        }
+        public abstract class Shape
+        {
+            public abstract void DrawWith(Graphics g);
+        }
 
-        
+        private void rb_CheckedChanged(object sender, EventArgs e)
+        {
+            IsShapeStart = !IsShapeStart;
+        }
     }
 }
